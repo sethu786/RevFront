e6f545aba58cdc8bfb7efd6587de08cd6573478c
sethu786 <75379267+sethu786@users.noreply.github.com>
2025-04-17 11:39:31 +0530

    Update index.js

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
 
     const fetchCategories = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/categories', {
+        const response = await fetch('https://revback-4.onrender.com/api/categories', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
