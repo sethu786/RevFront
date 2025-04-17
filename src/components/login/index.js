2595d51207b23fa63cb09a6972761003b34ae11f
sethu786 <75379267+sethu786@users.noreply.github.com>
2025-04-17 11:38:29 +0530

    Update index.js

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -19,7 +19,7 @@ const Login = () => {
     e.preventDefault();
     setErrorMsg('');
     try {
-      const res = await fetch('http://localhost:3000/api/auth/login', {
+      const res = await fetch('https://revback-4.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
